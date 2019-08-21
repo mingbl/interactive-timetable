143bb19a26fb72035bb5736bcae2cbe489b9aae0
Ben Leong <parkingcostsmoney@outlook.com>
2019-08-21 23:02:41 +1000

    Getting there. The timetable kind of works - Times don't show up and details can't be added.

--- public/scripts.js
+++ public/scripts.js
@@ -1,39 +1,12 @@
 //const version = "34c";
 //document.getElementById("version").innerHTML = "Version: " + version + " - Created by ML";
 
-const detailsForm = document.getElementById("details-form");
-var formRowIncrement = 1;
-function addSubject() {
-  formRowIncrement++;
-
-  let tr = document.createElement("tr");
-  tr.id = "subject-" + formRowIncrement;
-  detailsForm.appendChild(tr);
-
-  let tdNumber = document.createElement("td");
-  tdNumber.innerHTML = "#" + formRowIncrement;
-  tdNumber.style.fontWeight = "bold";
-  tr.appendChild(tdNumber);
-
-  let tdCode = document.createElement("td");
-  let inputCode = document.createElement("input");
-  inputCode.classList.add("inputs-" + formRowIncrement);
-  tr.appendChild(tdCode).appendChild(inputCode);
-
-  let tdName = document.createElement("td");
-  let inputName = document.createElement("input");
-  inputName.classList.add("inputs-" + formRowIncrement);
-  tr.appendChild(tdName).appendChild(inputName);
-
-  let tdLink = document.createElement("td");
-  let inputLink = document.createElement("input");
-  inputLink.classList.add("inputs-" + formRowIncrement);
-  tr.appendChild(tdLink).appendChild(inputLink);
-
-  let tdTeacher = document.createElement("td");
-  let inputTeacher = document.createElement("input");
-  inputTeacher.classList.add("inputs-" + formRowIncrement);
-  tr.appendChild(tdTeacher).appendChild(inputTeacher);
+const detailsForm = document.getElementById("details-form"),
+periodTimes = {
+  0: "8:00-9:10",
+  1: "9:10-10:30",
+  2: "10:55-12:05",
+  3: "12:50-2:00",
 };
 
 function saveDetails() {
@@ -58,3 +31,97 @@ function setControl() {
 function setSession(cell) {
   cell.innerHTML = "He";
 };
+
+function enableEditor() {
+
+}
+
+
+
+// SUPPORT FOR OLD VERSION
+var subject1 = localStorage.getItem("SUBJECT1").replace(/,-/g, ",null").split(","), subject2 = localStorage.getItem("SUBJECT2").replace(/,-/g, ",null").split(","), subject3 = localStorage.getItem("SUBJECT3").replace(/,-/g, ",null").split(","), subject4 = localStorage.getItem("SUBJECT4").replace(/,-/g, ",null").split(","), subject5 = localStorage.getItem("SUBJECT5").replace(/,-/g, ",null").split(","), subject6 = localStorage.getItem("SUBJECT6").replace(/,-/g, ",null").split(","), periods = localStorage.getItem("PERIODS").replace(/-/g, "null").split(",");
+var data = {
+  subjects: {
+    1: {
+      code: subject1[0],
+      name: subject1[1],
+      link: subject1[2],
+      teacher: subject1[3],
+      periods: [],
+    },
+    2: {
+      code: subject2[0],
+      name: subject2[1],
+      link: subject2[2],
+      teacher: subject2[3],
+      periods: [],
+    },
+    3: {
+      code: subject3[0],
+      name: subject3[1],
+      link: subject3[2],
+      teacher: subject3[3],
+      periods: [],
+    },
+    4: {
+      code: subject4[0],
+      name: subject4[1],
+      link: subject4[2],
+      teacher: subject4[3],
+      periods: [],
+    },
+    5: {
+      code: subject5[0],
+      name: subject5[1],
+      link: subject5[2],
+      teacher: subject5[3],
+      periods: [],
+    },
+    6: {
+      code: subject6[0],
+      name: subject6[1],
+      link: subject6[2],
+      teacher: subject6[3],
+      periods: [],
+    },
+  },
+  options: new Object(),
+};
+for (var s = 1; s < 7; s++) {
+  for (var p = 0; p < periods.length; p++) {
+    if (periods[p] == data.subjects[s].code) {
+      data.subjects[s].periods.push(p);
+    };
+  };
+};
+if (localStorage["USE-SCODES"]) {data.options["displayCodes"] = true};
+if (localStorage["USE-TNAMES"]) {data.options["displayNames"] = true};
+if (localStorage["USE-COLOURS"]) {
+  data.options["customColours"] = [localStorage.getItem("COLOUR-BG"), localStorage.getItem("COLOUR-TX")];
+};
+localStorage.setItem("data", JSON.stringify(data));
+
+
+
+
+const sessions = document.getElementsByClassName("sessions");
+function layoutTable() {
+  for (var s = 1; s <= 6; s++) {
+    let subject = data.subjects[s];
+    for (var p = 0; p < subject.periods.length; p++) {
+      let cell = sessions[subject.periods[p]];
+      cell.title = subject.name + " | " + subject.teacher;
+      cell.classList.add("valid");
+      let a = document.createElement("a");
+      a.href = subject.link;
+      a.classList.add("session-link");
+      cell.appendChild(a);
+      let div = document.createElement("div");
+      div.innerHTML += "<b>" + subject.name + "</b>";
+      if (data.options.hasOwnProperty("displayNames")) {div.innerHTML += "<br>" + subject.teacher};
+      if (data.options.hasOwnProperty("displayCodes")) {div.innerHTML += "<br>" + subject.code};
+      a.appendChild(div);
+    };
+  };
+};
+layoutTable()
